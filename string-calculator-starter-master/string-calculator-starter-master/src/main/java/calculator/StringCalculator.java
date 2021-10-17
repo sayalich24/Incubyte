37f0abc68faa47af8c01bf84ae45797c51cf1b67
sayalich24 <89347071+sayalich24@users.noreply.github.com>
2021-10-18 02:00:24 +0530

    test2

--- string-calculator-starter-master/string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -2,12 +2,13 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) 
+     public int add(String input) 
     {
-    	int a = 0; int b = 0; int sum = 0;
+    	int sum = 0;
     	StringTokenizer st = new StringTokenizer(input,",");
-    	if(st.hasMoreTokens())
+    	while(st.hasMoreTokens())
     	{
+    		int a = 0;
     		String temp = st.nextToken();
     		try
     		{
@@ -17,22 +18,12 @@ class StringCalculator {
     		{
     			a = 0;
     		}
+    		sum = sum + a;
     	}
-    	if(st.hasMoreTokens())
-    	{
-    		String temp = st.nextToken();
-    		try
-    		{
-    			b = Integer.parseInt(temp);
-    		}
-    		catch(Exception e)
-    		{
-    			b = 0;
-    		}
-    	}
-    	sum = a + b;
-//    	System.out.println(input+"  "+sum);
+    	
+    	System.out.println(input+"  "+sum);
         return sum;
     }
+
 
 }
